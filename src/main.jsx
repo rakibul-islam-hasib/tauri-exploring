1870c42401b1ebb8adb6b2b541050c2928624ea5
Hasib Hossain <boraborhasib@gmail.com>
2023-10-25 23:14:40 +0600

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import Login from "./pages/Login";
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
